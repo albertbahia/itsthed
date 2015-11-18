eb34e27b0ef9de1a1716a501f56fe3ffc241432d
albertbahia <albert.bahia1@gmail.com>
2015-11-17 22:24:45 -0500

    added hover event listener for anchor tags to implement underline styling

--- public/js/grayscale.js
+++ public/js/grayscale.js
@@ -48,7 +48,7 @@ function init() {
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -160,7 +160,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
@@ -175,4 +175,12 @@ function init() {
         map: map,
         icon: image
     });
+
+    $('a').hover(
+      function() {
+        $(this).css('border-bottom', '1px solid');
+      }, function() {
+        $(this).css('border-bottom', '');
+      }
+    );
 }
